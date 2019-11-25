fbd0270419e3daffefce5c8d04a930339d0ab947
Nick Pontorno <npontorno@outlook.com>
2019-11-25 16:35:10 -0500

    database update

--- data/accounts.js
+++ data/accounts.js
@@ -7,12 +7,35 @@ export default
         email: "address",
         password: "pass",
         isOwner: false,
-        postCount: 0,
-        posts: []
+        postCount: 2,
+        posts:
+        [
+            {
+                id: 3331,
+                itemId: 2221,
+                body: "some text about restaurant",
+                rating: 5,
+                upvotes: 0
+            },
+
+            {
+                id: 3332,
+                itemId: 2222,
+                body: "some more text about a different restaurant",
+                rating: 2,
+                upvotes: 3
+            }
+        ]
     },
 
     {
-        "id": 0,
-        "name": "myName0"
+        id: 0002,
+        first: "fname2",
+        last: "lname2",
+        email: "address2",
+        password: "pass2",
+        isOwner: true,
+        postCount: 0,
+        posts: []
     }
 ]
--- data/restaurants.js
+++ data/restaurants.js
@@ -1,12 +1,47 @@
 export default
 [
     {
-        "id": 0,
-        "name": "myName0"
+        id: 1111,
+        ownerId: 0002,
+        rating: 2.23,
+        name: "restaurantName",
+        city: "Chapel Hill",
+        state: "NC",
+        zip: 27516,
+        address: "address goes here",
+        cusine: "American",
+        items:
+        [
+            {
+                id: 2221,
+                name: "burger",
+                image: "url",
+                price: "$5.23",
+                rating: 4.5,
+                category: "Entree"
+            },
+
+            {
+                id: 2222,
+                name: "cocktail",
+                image: "url",
+                price: "$7.23",
+                rating: 3.2,
+                category: "Drink"
+            }
+        ]
     },
 
     {
-        "id": 0,
-        "name": "myName0"
+        id: 1112,
+        ownerId: 0002,
+        rating: 3.4,
+        name: "restaurantName",
+        city: "Chapel Hill",
+        state: "NC",
+        zip: 27516,
+        address: "address goes here",
+        cusine: "American",
+        items: []
     }
 ]
