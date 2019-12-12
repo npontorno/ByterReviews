846874e87132921eba55aa40e080b4c87ae1d267
Nick Pontorno <npontorno@outlook.com>
2019-12-12 03:13:48 -0500

    added

--- javascript/renderProcessClaim.js
+++ javascript/renderProcessClaim.js
@@ -11,20 +11,7 @@ $(async function()
 
     if (JSON.parse(localStorage.getItem("account")).isOwner.toString() == "false")
     {
-        alert("Only verified restaurant owners can add restaurants.");
+        alert("Only verified restaurant owners can add restaurants.")
         window.location.href = "http://localhost:3000/";
     }
-
-    $(document).on("click", ".signupbtn", function()
-    {
-        let loggedIn = localStorage.getItem("loggedIn").toString();
-
-        if (loggedIn == "false")
-        {
-            alert("You must be logged in to do that.");
-            return;
-        }
-
-        handlePostButtonPress();
-    })
-});
+})
